732e700588540d3ef70da60e1a4976e4551be521
morteza rasooli <motteza1182@gmail.com>
2025-03-15 11:34:16 +0330

    add canvas

--- v2/DigiClock_v2.py
+++ v2/DigiClock_v2.py
@@ -5,11 +5,13 @@ import json
 
 WINDOW_WIDTH: int
 WINDOW_HEIGHT: int
+BACKGROUND_COLOR: str
 
 with open("v2\Clock_info.json", 'r') as f:
     data = json.load(f)
     WINDOW_WIDTH = data["window_info"]["window_width"]
     WINDOW_HEIGHT = data["window_info"]["window_height"]
+    BACKGROUND_COLOR = data["canvas_info"]["background_color"]
  
 
 window = tk.Tk()
@@ -18,13 +20,12 @@ window_screenwidth = window.winfo_screenwidth()
 window_screenheight = window.winfo_screenheight()
 window_x = int((window_screenwidth/2)-(WINDOW_WIDTH/2))
 window_y = int((window_screenheight/2)-(WINDOW_HEIGHT/2))
-
 window.geometry(F"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{window_x}+{window_y}")
 
-# print(window_height)
-print(WINDOW_WIDTH)
+canvas = tk.Canvas(window, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg=BACKGROUND_COLOR)
+canvas.pack()
 
-# window.geometry(f"{int()}")
+
 
 
 tk.mainloop()
